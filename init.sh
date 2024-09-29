b1de42f7f778f81cfdb371fd118447e5f1a14c4b
lifegoeson <iamlzw@163.com>
2024-09-30 01:22:47 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -20,18 +20,8 @@ peer channel create -o orderer.example.com:7050 -c mychannel -f ./channel-artifa
 #peer0.org1 join channel
 peer channel join -b mychannel.block
 
-export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PROJECT_PATH}/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PROJECT_PATH}/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
 #install chaincode on peer0.org1
 peer chaincode install -n benchmark -v 1.0 -p chaincode/benchmark
-
-#export peer0.org1 env
-export CORE_PEER_LOCALMSPID="Org1MSP"
-export CORE_PEER_TLS_ROOTCERT_FILE=${PROJECT_PATH}/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export CORE_PEER_MSPCONFIGPATH=${PROJECT_PATH}/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
 
 #instantiate chaincode on peer0.org1
 peer chaincode instantiate -o orderer.example.com:7050 --tls --cafile ${PROJECT_PATH}/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n benchmark -v 1.0 -c '{"Args":["init"]}'
